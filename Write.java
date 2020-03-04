872cca7aec29b69a259f78386fb5b64ca858d240
SAUMILDHANKAR <babyyodanemo2020@gmail.com>
2020-03-04 13:49:09 -0800

    Add files via upload
    Test file.

---
+++ Write.java
@@ -0,0 +1,37 @@
+import java.io.*;
+
+public class Write {
+    public static void main(String [] args) {
+
+        // The name of the file to open.
+        String fileName = "Written.txt";
+
+        try {
+            // Assume default encoding.
+            FileWriter fileWriter =
+                new FileWriter(fileName);
+
+            // Always wrap FileWriter in BufferedWriter.
+            BufferedWriter bufferedWriter =
+                new BufferedWriter(fileWriter);
+
+            // Note that write() does not automatically
+            // append a newline character.
+            bufferedWriter.write("Hello there,");
+            bufferedWriter.write(" here is the text.");
+            bufferedWriter.newLine();
+            bufferedWriter.write("We are writing");
+            bufferedWriter.write(" the text to the file.");
+
+            // Always close files.
+            bufferedWriter.close();
+        }
+        catch(IOException ex) {
+            System.out.println(
+                "Error writing to file '"
+                + fileName + "'");
+            // Or we could just do this:
+            // ex.printStackTrace();
+        }
+    }
+}
