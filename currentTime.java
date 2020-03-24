50c34061289f955712b225f297fd7991f51dc0e0
SAUMILDHANKAR <babyyodanemo2020@gmail.com>
2020-03-24 12:25:38 -0700

    Add files via upload

---
+++ currentTime.java
@@ -0,0 +1,12 @@
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class currentTime {
+
+    public static void main(String[] args) {
+        Calendar cal = Calendar.getInstance();
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+        System.out.println( sdf.format(cal.getTime()) );
+    }
+
+}
